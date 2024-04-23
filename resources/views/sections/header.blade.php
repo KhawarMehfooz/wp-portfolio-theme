000d7a44653148aa944e5e1971a19e9d141acc02
Khawar Mehfooz <khawarmehfooz565@gmail.com>
2024-04-23 07:17:17 +0500

    update navbar styling

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -7,7 +7,7 @@
     <img class="w-14 h-14 rounded-full" src="{{get_field('portfolio_logo', 'option')}}" alt="{!! $siteName !!}" loading="lazy"/>
     </a>
     @if (has_nav_menu('primary_navigation'))
-      <nav class="nav-primary " aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      <nav class="nav-primary mx-auto md:ml-auto md:mr-0" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
       </nav>
     @endif
